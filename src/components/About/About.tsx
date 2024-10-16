463f9e3b003b79800b8c400b779a029b92148e53
Daniel <deco2449584@gmail.com>
2024-10-16 11:43:04 -0500

    añadi otras habilidades

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,20 +1,27 @@
 import { Container } from "./styles";
 import DanielCaro from "../../assets/perfil.png";
 import wordpress from "../../assets/wordpress.svg";
-import shopify from "../../assets/shopify.svg";
+import mongodb from "../../assets/mongodb.svg";
 import htmlIcon from "../../assets/html-icon.svg";
 import cssIcon from "../../assets/css-icon.svg";
 import jsIcon from "../../assets/js-icon.svg";
 import nodeIcon from "../../assets/node-icon.svg";
 import reactIcon from "../../assets/react-icon.svg";
 import typescriptIcon from "../../assets/typescript-icon.svg";
-import vueIcon from "../../assets/vue-icon.svg";
-import boostrapIcon from "../../assets/bootstrap-icon.svg";
+import angular from "../../assets/angular.svg";
+import kotlin from "../../assets/kotlin.svg";
 import ScrollAnimation from "react-animate-on-scroll";
 import { useTranslation } from "react-i18next";
-
+import mysql from "../../assets/mysql-icon.svg";
+import homeassistant from "../../assets/homeassistant.svg";
+import homekit from "../../assets/homekit.svg";
+import nodered from "../../assets/nodered.png";
+import arduino from "../../assets/arduino.png";
+import esp8266 from "../../assets/esp8266.png";
+import mqtt from "../../assets/mqtt.png";
+import cpp from "../../assets/C.svg";
 export function About() {
-  const { t } = useTranslation(); // Usa la función de traducción
+  const { t } = useTranslation();
 
   return (
     <Container id="about">
@@ -46,12 +53,12 @@ export function About() {
           </div>
           <div className="hability">
             <ScrollAnimation animateIn="fadeInUp" delay={0.12 * 1000}>
-              <img src={shopify} alt="shopify" />
+              <img src={mongodb} alt="mongodb" />
             </ScrollAnimation>
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
-              <img src={reactIcon} alt="React" />
+            <ScrollAnimation animateIn="fadeInUp" delay={0.12 * 1000}>
+              <img src={mysql} alt="mysql" />
             </ScrollAnimation>
           </div>
           <div className="hability">
@@ -60,8 +67,14 @@ export function About() {
             </ScrollAnimation>
           </div>
           <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
+              <img src={reactIcon} alt="React" />
+            </ScrollAnimation>
+          </div>
+
+          <div className="hability">
             <ScrollAnimation animateIn="fadeInUp" delay={0.15 * 1000}>
-              <img src={vueIcon} alt="Vue" />
+              <img src={angular} alt="angular" />
             </ScrollAnimation>
           </div>
           <div className="hability">
@@ -81,16 +94,58 @@ export function About() {
           </div>
           <div className="hability">
             <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
-              <img src={boostrapIcon} alt="bootstrap" />
+              <img src={jsIcon} alt="JavaScript" />
             </ScrollAnimation>
           </div>
           <div className="hability">
             <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
-              <img src={jsIcon} alt="JavaScript" />
+              <img src={kotlin} alt="kotlin" />
+            </ScrollAnimation>
+          </div>
+        </div>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.5 * 1000}>
+          <h3>{t("domotics_skills_title")}</h3>
+        </ScrollAnimation>
+        <div className="hard-skills">
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.1 * 1000}>
+              <img src={homeassistant} alt="Home Assistant" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.12 * 1000}>
+              <img src={homekit} alt="HomeKit" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.14 * 1000}>
+              <img src={nodered} alt="Node-RED" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
+              <img src={arduino} alt="Arduino" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
+              <img src={esp8266} alt="ESP8266" />
+            </ScrollAnimation>
+          </div>
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.2 * 1000}>
+              <img src={mqtt} alt="MQTT" />
+            </ScrollAnimation>
+          </div>
+
+          <div className="hability">
+            <ScrollAnimation animateIn="fadeInUp" delay={0.24 * 1000}>
+              <img src={cpp} alt="C++" />
             </ScrollAnimation>
           </div>
         </div>
       </div>
+
       <div className="about-image">
         <ScrollAnimation animateIn="fadeInRight" delay={0.2 * 1000}>
           <img
